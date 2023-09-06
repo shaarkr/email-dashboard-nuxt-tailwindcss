727f2f808694681b7a3c6744b087097164b2b137
Sharath Kumar <sharathkr@protonmail.com>
2023-09-06 17:10:19 +0530

    add "Inter" font to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,14 @@ export default defineNuxtConfig({
   typescript: { shim: false },
   modules: ['@davestewart/nuxt-scrollbar', 'nuxt-icon', '@nuxtjs/device'],
   css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      title: 'Nuxt 3 - Email Dashboard',
+      link: [
+        { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+        { rel: 'preconnect', href: 'https://rsms.me/' },
+      ],
+    },
+  },
 });
