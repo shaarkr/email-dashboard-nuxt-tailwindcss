e70958b205f324daed7090090c4a07a579d04b02
Sharath Kumar <sharathkr@protonmail.com>
2023-09-06 17:07:44 +0530

    add nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  typescript: { shim: false },
+  modules: ['@davestewart/nuxt-scrollbar', 'nuxt-icon', '@nuxtjs/device'],
+});
