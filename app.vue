b0ccc0128c9de274f6164610490c86df6d9ae215
Sharath Kumar <sharathkr@protonmail.com>
2023-09-06 23:39:04 +0530

    add details header

--- app.vue
+++ app.vue
@@ -5,6 +5,14 @@
 
     <!-- Message List Area -->
     <InboxMessage />
+
+    <!-- Message Details -->
+    <div
+      v-if="msgOpen"
+      class="h-screen relative md:w-full w-screen transition border-l border-zinc-800">
+      <!-- Heading of Details -->
+      <InboxDetailsHeading />
+    </div>
   </div>
 </template>
 
@@ -14,4 +22,7 @@
       class: "bg-zinc-950 text-zinc-50 min-h-screen",
     },
   });
+
+  // state of selected message
+  const msgOpen = useState("msgOpen", () => false);
 </script>
---
+++ components/Inbox/Details/Heading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-end gap-3 py-5 px-3 border-b border-zinc-800">
+    <button
+      v-tooltip="'Ask for help'"
+      class="text-zinc-500 hover:text-zinc-100 transition">
+      <Icon name="solar:question-circle-bold-duotone" size="22" />
+    </button>
+    <button
+      v-tooltip="'View settings'"
+      class="text-zinc-500 hover:text-zinc-100 transition">
+      <Icon name="solar:settings-bold-duotone" size="22" />
+    </button>
+    <button v-tooltip="'Open menu'" class="text-zinc-500 hover:text-zinc-100 transition">
+      <Icon name="solar:menu-dots-square-bold-duotone" size="22" />
+    </button>
+    <img
+      v-tooltip="'Update profile'"
+      src="https://picsum.photos/80/80"
+      alt="Logged in user"
+      class="rounded-full ml-3 w-6 h-6 shrink-0 object-cover ring-1 ring-zinc-800/30" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
