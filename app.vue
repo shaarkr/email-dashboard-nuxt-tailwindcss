32b9b14527f52d01ff64b975930878012e1d1545
Sharath Kumar <sharathkr@protonmail.com>
2023-09-07 22:57:33 +0530

    add email header & sender details

--- app.vue
+++ app.vue
@@ -12,6 +12,28 @@
       class="h-screen relative md:w-full w-screen transition border-l border-zinc-800">
       <!-- Heading of Details -->
       <InboxDetailsHeading />
+      <div class="px-10 py-7 border-b border-zinc-800">
+        <!-- Sender details -->
+        <div class="flex flex-col md:flex-row md:items-center justify-between gap-4 mt-3">
+          <div class="flex items-center gap-5">
+            <img
+              src="https://picsum.photos/id/220/80"
+              alt="User"
+              class="object-cover rounded-full w-10 h-10 ring-1 ring-zinc-700/10" />
+            <div>
+              <p class="text-sm -mb-1">John Doe</p>
+              <a href="mailto:johnathandoe@gmail.com" class="text-xs text-zinc-500"
+                >johnathandoe@gmail.com</a
+              >
+            </div>
+          </div>
+          <div>
+            <span class="text-xs text-zinc-500 font-medium"
+              >Today 10:17 PM (3hr ago)</span
+            >
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- components/Inbox/Details/Heading.vue
+++ components/Inbox/Details/Heading.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex items-center justify-end gap-3 py-5 px-3 border-b border-zinc-800">
+  <div
+    class="flex items-center justify-end gap-3 py-5 px-3 border-b border-1 border-zinc-800">
     <button
       v-tooltip="'Ask for help'"
       class="text-zinc-500 hover:text-zinc-100 transition">
@@ -19,6 +20,63 @@
       alt="Logged in user"
       class="rounded-full ml-3 w-6 h-6 shrink-0 object-cover ring-1 ring-zinc-800/30" />
   </div>
+
+  <div class="border-b border-zinc-800 px-3 py-3 flex items-center justify-between">
+    <div class="flex items-center gap-3">
+      <button
+        @click="msgOpen = false"
+        tooltip="'Go back'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:arrow-left-line-duotone" size="18" />
+      </button>
+      <button
+        tooltip="'Archive this email'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:archive-check-line-duotone" size="18" />
+      </button>
+      <button
+        tooltip="'Delete this email'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:trash-bin-minimalistic-line-duotone" size="18" />
+      </button>
+    </div>
+
+    <div class="flex items-center gap-3">
+      <button
+        tooltip="'View previous'"
+        class="text-zinc-500 inline-flex items-center hover:text-zinc-200 transition cursor-not-allowed">
+        <Icon name="solar:round-alt-arrow-left-bold-duotone" size="20" />
+      </button>
+      <div class="text-xs text-zinc-400">1 of 485</div>
+      <button
+        tooltip="'View next email'"
+        class="text-zinc-200 inline-flex items-center hover:text-zinc-200 transition">
+        <Icon name="solar:round-alt-arrow-right-bold-duotone" size="20" />
+      </button>
+    </div>
+
+    <div class="flex items-center gap-3">
+      <button
+        @click="msgOpen = false"
+        tooltip="'Print email'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:printer-2-outline" size="18" />
+      </button>
+      <button
+        tooltip="'Oenn in new window'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:arrow-right-up-line-duotone" size="18" />
+      </button>
+      <button
+        tooltip="'More options'"
+        class="text-zinc-500 hover:text-zinc-200 transition">
+        <Icon name="solar:menu-dots-square-line-duotone" size="18" />
+      </button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  // state of selected message
+  const msgOpen = useState("msgOpen", () => false);
+</script>
